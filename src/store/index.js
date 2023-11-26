d71f3fc7d2fc18099e3dfbaaa3b2a46ba13f2a61
jknb <alvertischris@gmail.com>
2023-11-27 00:51:12 +0200

    Delete row store

--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,5 @@ import useModalsStore from "./modals";
 import useThemeStore from "./theme";
 import useGameStatusStore from "./gameStatus";
 import useAppStore from "./app";
-import useRowStore from "./row";
 
-export {
-  useModalsStore,
-  useThemeStore,
-  useGameStatusStore,
-  useAppStore,
-  useRowStore,
-};
+export { useModalsStore, useThemeStore, useGameStatusStore, useAppStore };
