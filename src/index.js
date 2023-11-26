4bb518d738328cb50ec291d770ef28d542808b7c
jknb <alvertischris@gmail.com>
2023-11-27 00:45:06 +0200

    Remove Context

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import ContextWrapper from "./ContextWrapper";
 import "./themes.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <ContextWrapper>
-      <App />
-    </ContextWrapper>
+    <App />
   </React.StrictMode>
 );
