609429ca22569bd732a6223732968962964289f3
jknb <alvertischris@gmail.com>
2023-11-27 13:06:34 +0200

    Move Rows to own component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,17 @@
 import "./App.css";
-import Row from "./Row";
-import { COLORS } from "./constants";
-import { useComponentListCreator } from "./hooks/useComponentListCreator";
-import { useAppStore, useThemeStore } from "./store";
+import { useThemeStore } from "./store";
 import Header from "./Header";
 import Modals from "./Modals";
+import Rows from "./Rows";
 
 function App() {
   const { theme } = useThemeStore();
-  const { code, gameKey, settings } = useAppStore();
-
-  const numberOfRows = settings.numberOfGuesses;
-  const rows = useComponentListCreator(Row, numberOfRows);
-
-  console.log(code.map((n) => `${COLORS[n]} - ${n}`));
-
   return (
     <div>
       <div className={`app-container ${theme}-theme`}>
         <Header />
         <hr className="separator" />
-        <div key={gameKey} className="game">
-          {rows}
-        </div>
+        <Rows />
         <Modals />
       </div>
     </div>
