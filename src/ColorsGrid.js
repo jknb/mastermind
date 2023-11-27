850dd836076e73761644b46b49baaa832b65f38b
jknb <alvertischris@gmail.com>
2023-11-27 11:49:22 +0200

    Rename component

--- src/ColorsGrid.js
+++ src/ColorsGrid.js
@@ -1,19 +1,21 @@
 import { useState } from "react";
 import GridCircle from "./GridCircle";
-import { repeatComponent } from "./utils";
 import { COLORS } from "./constants";
 import "./ColorsGrid.css";
+import { useComponentListCreator } from "./hooks/useComponentListCreator";
 
-function ColorsGrid({ onColorSelect, backgroundColor }) {
+function CodePegsSelectorGrid({ onColorSelect, backgroundColor }) {
   const [color, setColor] = useState(backgroundColor);
 
   const onGridCircleClick = (index) => {
-    const newColor = color === COLORS[index] ? "white" : COLORS[index];
+    const selectedColor = COLORS[index];
+    const newColor = color === selectedColor ? "white" : selectedColor;
+
     onColorSelect(newColor);
     setColor(newColor);
   };
 
-  const gridCircles = repeatComponent(GridCircle, 9, (index) => ({
+  const gridCircles = useComponentListCreator(GridCircle, 9, (index) => ({
     size: "24px",
     backgroundColor: COLORS[index],
     onClick: () => onGridCircleClick(index),
@@ -22,4 +24,4 @@ function ColorsGrid({ onColorSelect, backgroundColor }) {
   return <div className="colors-grid-container">{gridCircles}</div>;
 }
 
-export default ColorsGrid;
+export default CodePegsSelectorGrid;
