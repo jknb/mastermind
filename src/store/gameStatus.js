7985cd9b95d339c8b7d0df8a7d2f6a5c6a61af5a
jknb <alvertischris@gmail.com>
2023-11-27 11:49:35 +0200

    Add `endGame` method

--- src/store/gameStatus.js
+++ src/store/gameStatus.js
@@ -1,8 +1,14 @@
 import { create } from "zustand";
+import useModalsStore from "./modals";
+import { MODALS } from "../constants";
 
-const useGameStatusStore = create((set) => ({
+const useGameStatusStore = create((set, get) => ({
   gameStatus: { finished: false, won: false },
   setGameStatus: (newGameStatus) => set(() => ({ gameStatus: newGameStatus })),
+  endGame: (isWinner) => {
+    get().setGameStatus({ finished: true, won: isWinner });
+    useModalsStore.getState().toggleModal(MODALS.GAMEOVER, true);
+  },
 }));
 
 export default useGameStatusStore;
