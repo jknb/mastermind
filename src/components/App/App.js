b46f102563ccad8481ccbe7e93c865d422f622fe
jknb <alvertischris@gmail.com>
2023-11-30 02:30:30 +0200

    Show selected mode on screen

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,18 @@
 import "./App.css";
-import { useThemeStore } from "../../store";
+import { useAppStore, useThemeStore } from "../../store";
 import Modals from "../Modals";
 import Header from "../Header";
 import Rows from "../Rows";
 
 function App() {
   const { theme } = useThemeStore();
+  const selectedGameModeId = useAppStore((state) => state.settings.id);
   return (
     <div>
       <div className={`app-container ${theme}-theme`}>
         <Header />
         <hr className="separator" />
+        <div className="selected-mode-display">Mode: {selectedGameModeId}</div>
         <Rows />
         <Modals />
       </div>
