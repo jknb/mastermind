62cb6d575be8459720d4f97dbf7cd6f71ba83de5
jknb <alvertischris@gmail.com>
2025-05-18 22:52:06 +0300

    fix: styles

--- src/store/theme.js
+++ src/store/theme.js
@@ -4,13 +4,13 @@ import { LOCAL_STORAGE_KEYS, THEMES } from "../constants";
 const { $LOCAL_THEME } = LOCAL_STORAGE_KEYS;
 
 const useThemeStore = create((set) => ({
-  theme: JSON.parse(localStorage.getItem($LOCAL_THEME)) || THEMES.DARK,
+  theme: localStorage.getItem($LOCAL_THEME) || THEMES.DARK,
   toggleTheme: () =>
     set((state) => {
       const nextTheme =
         state.theme === THEMES.LIGHT ? THEMES.DARK : THEMES.LIGHT;
 
-      localStorage.setItem($LOCAL_THEME, JSON.stringify(nextTheme));
+      localStorage.setItem($LOCAL_THEME, nextTheme);
       return {
         theme: nextTheme,
       };
